659d514e2c6b3283c7b394d4a4574105e029538c
Elizabeth Doyle <123044077+elizabethmdoyle@users.noreply.github.com>
2024-02-17 19:26:44 -0500

    5 out of 17 passing tests

--- api/server.js
+++ api/server.js
@@ -1,2 +1,23 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+//SET ROUTES 
+// const postRouter = require('./posts/posts-router');
+
+// server.use('post routes', postRouter);
+
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'Endpoint does not exist'
+    })
+})
+
+
+
+module.exports = server
